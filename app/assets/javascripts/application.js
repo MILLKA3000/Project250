9c07d3ac60620508134c6d0ba225e0ec7a211509
MILLKA3000 <s.tchekanov@svitla.com>
2014-06-20 19:42:12 +0300

    3 task

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,20 +1,14 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
 //= require jquery_ujs
-//= require_tree
-$(function() {
+//= require_tree .
+$(document).ready(function(){
     $( "input[type=submit], a, button" ).button()
     $( ".sort a" ).button( "destroy" );
     $( ".datepicker" ).datepicker();
+
+    $(document).on("click",".sort a", function ( event ) {
+            $.getScript(this.href);
+            return false;
+        });
+
 
 });
